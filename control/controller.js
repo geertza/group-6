ab10fdb8bb4c652ae746651f79a6ff1726d2cd46
geertza <geertza@gmail.com>
2020-06-14 15:25:57 -0600

    finally nailed the search

--- control/controller.js
+++ control/controller.js
@@ -4,22 +4,17 @@ var model = require('../models/model')
 let fs=require("fs")
 // Create all our routes and set up logic within those routes where required.
 router.post("/api", function(req, res) {
-//     // model.create(["spoon"],
-//     let {search,cuisine,diet,alergy} = req.body
-//     console.log("control allergy=",alergy) 
-//     model.FindRecipe(search,cuisine,diet,alergy,function (res){
-//     console.log(res.data)
-//       data =`const searchResults = ${JSON.stringify(res.data.results)}`
-//     console.log("dats ="+data)
-//       fs.writeFileSync("./public/javascript/apiResults.js", data, function(err) {
-//       if(err) {
-//            console.log(err);
-//       }
-      res.render("ApiResults")
+    let {search,cuisine,diet,alergy} = req.body
+  
+    model.FindRecipe(search,cuisine,diet,alergy,function (results){
+      data =`const searchResults = ${JSON.stringify(results.data.results)}`
+      fs.writeFile("./public/javascript/apiResults.js", data, (err) => {
+        if (err) throw err;
+        
   }); 
-
-// });
-// });
+res.render("ApiResults")
+});
+});
   
 
 router.get("/", (req, res) => {
