fe59575961c638bcfd72cee5ca5b7e84990f989e
geertza <geertza@gmail.com>
2020-06-15 12:36:36 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,9 @@
 const Sequelize = require("sequelize");
+const { connect } = require("../control/controller");
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConection(process.env.JAWSDB_URL);  
+}else{
 
 const sequelize = new Sequelize("kitchenBoss", "root", "password", {
   host: "localhost",
@@ -9,5 +14,5 @@ const sequelize = new Sequelize("kitchenBoss", "root", "password", {
     min: 0,
     idle: 10000,
   },
-});
+});}
 module.exports = sequelize;
